b6f90640f0f1400c9ee2199cec9ed8d35dc5b5d2
markosmgm4281-blip <markosmgm4281@gmail.com>
2025-12-04 23:29:36 -0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,16 @@
+import Head from 'next/head'
 import '../styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#020617" />
+        <link rel="apple-touch-icon" href="/icon-192.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
