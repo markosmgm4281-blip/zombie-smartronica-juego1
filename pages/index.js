308acf13ff6f11179354c14bc56e19a7a8dbdfdf
markosmgm4281-blip <markosmgm4281@gmail.com>
2025-12-06 20:28:20 -0300

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+export default function Home() {
+  return (
+    <div style={{ 
+      height: "100vh", 
+      display: "flex", 
+      justifyContent: "center", 
+      alignItems: "center", 
+      background: "black", 
+      color: "white", 
+      fontSize: "24px",
+      flexDirection: "column"
+    }}>
+      <h1>🧟 Zombie Smartronica</h1>
+      <p>El juego ya está vivo ✅</p>
+    </div>
+  );
+}
